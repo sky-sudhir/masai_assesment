97d2584775d0050e21bf97130de928fb84d64b35
Sudhir <sky875025@gmail.com>
2025-08-19 18:31:45 +0530

    changes

--- api/router/lang.py
+++ api/router/lang.py
@@ -3,6 +3,7 @@ from unittest import result
 from fastapi import APIRouter
 from pydantic import BaseModel
 from api.db.chromadb import collection
+from langchain_groq import ChatGroq
 
 
 
@@ -17,7 +18,26 @@ async def ask_ques(que:Question):
     n_results=4 
     )
 
-    return results
+    llm = ChatGroq(
+    model="deepseek-r1-distill-llama-70b",
+    temperature=0,
+    max_tokens=None,
+    reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+    
+    # other params...
+)
+    messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+    ai_msg = llm.invoke(messages)
+
+    return ai_msg
 
 
 
